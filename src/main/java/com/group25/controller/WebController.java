570e69757208adde8e14f07d91e58be9b388124f
Thayakaran <thayakrn@gmail.com>
2018-09-25 13:25:25 +0530

    changed frontend architecture working with spring boot

---
+++ src/main/java/com/group25/controller/WebController.java
@@ -0,0 +1,21 @@
+package com.group25.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping(value="/")
+    public String loginPage(){
+        return "login";
+    }
+    @GetMapping(value="/home.html")
+    public String homePage(){
+        return "home";
+    }
+    @GetMapping(value="/registration.html")
+    public String registrationPage(){
+        return "registration";
+    }
+}
