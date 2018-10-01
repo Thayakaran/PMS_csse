04434f178365050bbe20a4ab1f3e22c63711283f
Thayakaran <thayakrn@gmail.com>
2018-10-01 08:43:04 +0530

    implemented encryption for password when register users

--- src/main/java/com/group25/dao/UserDao.java
+++ src/main/java/com/group25/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.group25.dao;
 
 import com.group25.entity.User;
+import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -85,9 +86,16 @@ public class UserDao {
         String wAddress = user.getwAddress();
         String role = user.getRole();
         String email = user.getEmail();
-        String password = user.getPassword();
+        String password = passwordEncrypt(user.getPassword());
         int res = jdbcTemplate.update(sql, new Object[] {fName, lName, mPhone, oPhone, hAddress, wAddress, role, email, password});
         return res;
 //        System.out.println("resposnessssssssssssssssssssssssss issssssssss : "+res);
+    }
+
+    public String passwordEncrypt(String pass){
+        StrongPasswordEncryptor passwordEncryptor = new StrongPasswordEncryptor();
+        String encryptedPassword = passwordEncryptor.encryptPassword(pass);
+
+        return encryptedPassword;
     }
 }
