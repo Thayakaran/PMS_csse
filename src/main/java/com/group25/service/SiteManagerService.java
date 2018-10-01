23bb42262b9b120bb2af00e856e10626b97f9dc5
JegaVithu <jeganathan.vithusan@my.sliit.lk>
2018-10-01 23:02:00 +0530

    updated sitemanagerservice

--- src/main/java/com/group25/service/SiteManagerService.java
+++ src/main/java/com/group25/service/SiteManagerService.java
@@ -1,9 +1,7 @@
 package com.group25.service;
 
 import com.group25.dao.SiteManagerDao;
-import com.group25.dao.UserDao;
 import com.group25.entity.SiteManager;
-import com.group25.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
