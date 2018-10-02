c526d24e06d1e3f771ba3754b29e1a5b7b41193d
Sankeeth Naguleswaran <n.sankeeth@gmail.com>
2018-10-02 14:04:13 +0530

    Create payments page and add script to the page

--- src/main/resources/static/js/viewJS/payments.js
+++ src/main/resources/static/js/viewJS/payments.js
@@ -1,13 +1,89 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  getAllData();
 
-    getAllData();
+  function getAllData() {
+    console.log("Get Data");
+
+    $.ajax({
+      type: "GET",
+      url: "/invoices/unpaid",
+      success: function(result) {
+
+          $('#datatable-responsive_unpaid').DataTable({
+              "data" : result,
+              "columns" : [
+                  { "data" : "invoiceID" },
+                  { "data" : "paymentStatus" },
+                  { "data" : "orderID" },
+                  { "data" : "date" },
+                  { "data" : "item" },
+                  { "data" : "supplier" },
+                  { "data" : "quantity" },
+                  { "data" : "orderedBy" },
+                  { "data" : "totalAmount" },
+                  { "data" : "discount" },
+                  { "data" : "netAmount" },
+                  { "data" : "contactNo" },
+                  {"defaultContent" : "<button id='btnDetails' class='btn btn-success' > Pay </button>"}
+              ],
+              "bDestroy": true
+          });
+
+      },
+      error: function(e) {
+        swal({
+          title: "Error",
+          text: "Unable to login to the system, Try again later!!",
+          type: "error"
+        });
+      }
+    });
 
 
+      $.ajax({
+          type: "GET",
+          url: "/invoices/paid",
+          success: function(result) {
 
-    function getAllData() {
+              $('#datatable-responsive_paid').DataTable({
+                  "data" : result,
+                  "columns" : [
+                      { "data" : "invoiceID" },
+                      { "data" : "paymentStatus" },
+                      { "data" : "orderID" },
+                      { "data" : "date" },
+                      { "data" : "item" },
+                      { "data" : "supplier" },
+                      { "data" : "quantity" },
+                      { "data" : "orderedBy" },
+                      { "data" : "totalAmount" },
+                      { "data" : "discount" },
+                      { "data" : "netAmount" },
+                      { "data" : "contactNo" }
+                  ],
+                  "bDestroy": true
+              });
 
-        console.log("Get Data");
+          },
+          error: function(e) {
+              swal({
+                  title: "Error",
+                  text: "Unable to get data, Try again later!!",
+                  type: "error"
+              });
+          }
+      });
 
-    }
+      $('#datatable-responsive_unpaid').on('click', '#btnDetails', function () {
+          var table = $('#datatable-responsive_unpaid').DataTable();
+          var data = table.row($(this).parents('tr')).data();
 
+          localStorage.setItem('invoiceID', data["invoiceID"]);
+          localStorage.setItem('netAmount', data["netAmount"]);
+
+          location.href = "processPayment";
+
+      });
+
+  }
 });
