2105dd2e47aee6344fb901361ca83269108a3109
Sankeeth Naguleswaran <n.sankeeth@gmail.com>
2018-10-02 16:53:16 +0530

    Validate form and fix the issues in the script

--- src/main/resources/static/js/viewJS/processPayment.js
+++ src/main/resources/static/js/viewJS/processPayment.js
@@ -2,39 +2,42 @@ $(document).ready(function () {
 
     var invoiceID = localStorage.getItem("invoiceID");
 
-    $('#invoiceID').html(invoiceID);
-    $('#amount').html(localStorage.getItem("amount"));
+    $('#invoiceID').val(invoiceID);
+    $('#amount').val(localStorage.getItem("amount"));
 
     localStorage.clear();
 
     $("#paymentForm").submit(function(event) {
-        event.preventDefault();
+        if(this.checkValidity())
+        {
+            event.preventDefault();
 
-        var paymentDetails = {
-            cardholerName : $("#cardholerName").val(),
-            cardNo :  $("#cardNo").val(),
-            expDate : $("#expDate").val(),
-            cvv :  $("#cvv").val(),
-            amount : $("#amount").val()
-        }
-
-        $.ajax({
-            type: "POST",
-            contentType: "application/json; charset=utf-8",
-            url: "/pay",
-            data: JSON.stringify(paymentDetails),
-            dataType: 'json',
-            success: function (res) {
-                if (res.error) {
-                    swal({title: "Error", text: res.error, type: "error"});
-                }
-                else {
-                    swal({title: "Success", text: "Payment Sent", type: "success"});
-                    ('#resetButton').click();
-                }
+            var paymentDetails = {
+                cardholerName : $("#cardholerName").val(),
+                cardNo :  $("#cardNo").val(),
+                expDate : $("#expDate").val(),
+                cvv :  $("#cvv").val(),
+                amount : $("#amount").val()
             }
-        });
 
+            $.ajax({
+                type: "POST",
+                contentType: "application/json; charset=utf-8",
+                url: "/pay",
+                data: JSON.stringify(paymentDetails),
+                dataType: 'json',
+                success: function (res) {
+                    if (res.error) {
+                        swal({title: "Error", text: res.error, type: "error"});
+                    }
+                    else {
+                        swal({title: "Success", text: "Payment Sent", type: "success"});
+                        ('#resetButton').click();
+                    }
+                }
+            });
+
+        }
 
     });
 
